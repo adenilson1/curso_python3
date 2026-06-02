921a551f9b4143e944bdc9ceca075d079877c81c
adenilson1 <adenilson99@hotmail.com>
2026-06-02 17:53:28 -0300

    Função print()

---
+++ projeto/aula02.py
@@ -0,0 +1,5 @@
+# Função print: argumentos nomeados sep= e and=
+print(12, 34, 1011, sep='-', end='\n##') # sep é um separador e end é o final de linha
+print(56, 78, sep="-", end='\n')
+print(9, 10, sep="-", end='\n')
+
