ca69b71dac85448ac2ec49b873b6636a01d71400
adenilson1 <adenilson99@hotmail.com>
2026-06-04 19:01:28 -0300

    Tipo int e float

---
+++ projeto/aula04.py
@@ -0,0 +1,19 @@
+# Tipos int e float
+
+print(11) # int
+print(-11) # int
+print(0) # int
+
+print(1.1, 10.11) # float
+print(0.0, -1.5) # float
+
+print('-' * 50)
+# classe type
+
+print(type(0))
+print(type(1))
+print(type(-1))
+print(type(0.0))
+print(type(1.1))
+print(type(-1.1))
+print(type('Otávio'))
