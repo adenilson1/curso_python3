0eac5ab4d35292eebf65b8e28acee953889d6fb4
adenilson1 <adenilson99@hotmail.com>
2026-06-07 16:35:10 -0300

    Introdução às variáveis

---
+++ projeto/aula07.py
@@ -0,0 +1,11 @@
+# nome_completo = 'Luiz Otávio Miranda'
+# soma_dois_mais_dois = 2 + 2
+# int_um = int('1')
+# print(int_um, type(int_um))
+# print(nome_completo, soma_dois_mais_dois)
+
+nome = 'Luiz'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome: ', nome, 'Idade: ', idade)
+print('É maior? ',maior_de_idade)
