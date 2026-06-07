ac380c750047b668da99e5808af46ecfd7084cf1
adenilson1 <adenilson99@hotmail.com>
2026-06-07 16:43:46 -0300

    Exercício: variáveis e tipos de dados

---
+++ projeto/aula08.py
@@ -0,0 +1,12 @@
+nome = "Carlos"
+sobrenome = "Silvia"
+idade = 15
+ano_nascimento = 2026-15
+maior_de_idade = idade >= 18
+altura = 1.7
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade:', idade)
+print('Ano nascimento:', ano_nascimento)
+print('É maior de idade:', maior_de_idade)
+print('Altura em metros:', altura)
